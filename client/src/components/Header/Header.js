ddbac8fcba9a46116ab895db6fa7c69ce197b99c
Nathan Dalton <ndaltondev@gmail.com>
2018-06-22 00:04:55 -0700

    added components for Header, Footer, Catalog and Cart.Also built the Header with responsive navbar.

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import ResponsiveMenu from 'react-responsive-navbar';
+import Icon from 'react-icons-kit';
+import {ic_dehaze} from 'react-icons-kit/md/ic_dehaze';
+import {ic_close} from 'react-icons-kit/md/ic_close';
+import './Header.css';
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="outter-logo">Ecomm</h1>
+            <ResponsiveMenu
+                menuOpenButton={<Icon size={26} icon={ic_dehaze} />}
+                menuCloseButton={<Icon size={26} icon={ic_close} />}
+                changeMenuOn="500px"
+                largeMenuClassName="large-menu"
+                smallMenuClassName="small-menu"
+                menu={
+                    <div className="header">
+                        <div className="logo">
+                            <h1>Ecomm</h1>
+                        </div>
+                        <nav>
+                            <ul>
+                                <li>Home</li>
+                                <li>Cart</li>
+                                <li>Login/Sign up</li>
+                            </ul>
+                        </nav>
+                    </div> 
+                }  
+            />
+            </div>
+        )
+    }
+}
